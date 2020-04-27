6ce0c49ecd7558ea86ff258b96617c9cdfb4cfed
Gaurav Sharma <kvdgaurav@gmail.com>
2020-04-26 23:55:57 -0700

    Updates

---
+++ precache-manifest.e56107f8fb4047834c47eb4aa6f89d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1738e5fb182a5f82745b62758c9ddd35",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "9c48f971b2eee998fa3c",
+    "url": "/quote-generator/static/css/main.3eb62a30.chunk.css"
+  },
+  {
+    "revision": "f140d386732b53e05d92",
+    "url": "/quote-generator/static/js/2.8d2bde29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.8d2bde29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c48f971b2eee998fa3c",
+    "url": "/quote-generator/static/js/main.779b5605.chunk.js"
+  },
+  {
+    "revision": "4de2143d3a3f64fb11e3",
+    "url": "/quote-generator/static/js/runtime-main.fc213f3e.js"
+  }
+]);
